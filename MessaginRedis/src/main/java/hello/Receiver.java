f3d289f5f79f0407ce4b5a5eec7913b2b4ee1d15
JungSu Kim <zzragida@gmail.com>
2016-07-04 01:04:39 +0900

    add LDAP authentication, Redis message receiver

---
+++ MessaginRedis/src/main/java/hello/Receiver.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Receiver {
+    private static final Logger log = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        log.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}
